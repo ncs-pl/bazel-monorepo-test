e4ee04c14f5829e095b57660c2a549f35baee7b5
Nicolas <git.n1c00o@gmail.com>
2022-06-26 18:13:44 +0200

    feat(rust): add simple web server which uses adder and printer

---
+++ web_server_rust/README.md
@@ -0,0 +1,3 @@
+# Web Server
+
+A simple web server written in Rust using `printer_rust` and `adder_rust`
---
+++ web_server_rust/src/main.rs
@@ -0,0 +1,47 @@
+use std::net::SocketAddr;
+
+use axum::extract::Path;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::Router;
+use axum::routing::get;
+use tracing::metadata::LevelFilter;
+
+use adder_rust::{add, add_two_random};
+
+#[tracing::instrument]
+async fn add_router(Path((a, b)): Path<(usize, usize)>) -> impl IntoResponse {
+    tracing::info!("summing {} and {}", a, b);
+
+    let res: String = format!("{} + {} = {}", a, b, add(a, b));
+
+    printer_rust::print(res.as_str());
+    (StatusCode::OK, res)
+}
+
+#[tracing::instrument]
+async fn add_random_router() -> impl IntoResponse {
+    tracing::info!("summing random numbers");
+
+    let res: String = format!("{}", add_two_random());
+
+    printer_rust::print(res.as_str());
+    (StatusCode::OK, res)
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .pretty()
+        .init();
+
+    let app: Router = Router::new()
+        .route("/random", get(add_random_router))
+        .route("/add/:a/:b", get(add_router));
+
+    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    tracing::info!("listening on {}", &addr);
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+}
